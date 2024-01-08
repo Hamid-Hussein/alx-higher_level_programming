426deb9b032e0280127eb8124ae073f86d27f1f9
Hamid-Hussein <hamidhussein72@gmail.com>
2024-01-08 13:16:29 -0800

    my comment

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return None
+    for I in reversed(my_list):
+        print(“{:d}”.format(i))
+
---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return None
+    for submatrix in matrix:
+        if len(submatrix)  == 0:
+            Print()
+            for i in range (len(submatrix) ):
+                Print (“{:d}”.format(submatrix[i])),end=”\n” if I is len (submatrix) 
+
